19fdd24f05898209edae97a6834cb3ef49a20f1e
Ranjitha1412 <51219884+Ranjitha1412@users.noreply.github.com>
2019-07-04 07:06:53 +0530

    Update s1.py

--- s1.py
+++ s1.py
@@ -7,4 +7,7 @@ for i in range(0,len(li)):
       if(li[i]==li[j]):
         final_li.append(li[j])
 final_li=sorted(final_li)
-print(" ".join(final_li))
+if(len(final_li)==0):
+  print("unique")
+else:
+  print(" ".join(final_li))
